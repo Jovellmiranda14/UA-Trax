8e29361aeedec06bd3048a3b8f1cdae15125e81a
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-08-27 14:26:51 +0800

    Department - Issue
    Fixed Null Value

--- database/migrations/2024_08_25_142116_create_ticket_queues_table.php
+++ database/migrations/2024_08_25_142116_create_ticket_queues_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('subject');
             $table->enum('status', ['Open', 'Resolved', 'In progress', 'Closed'])->default('Open')->nullable();
             $table->enum('priority', ['Moderate', 'Urgent', 'Low', 'High'])->default('Moderate')->nullable();
-            $table->enum('department', ['SAS', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE']);
+            $table->enum('department', ['SAS', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE'])->nullable();
             $table->string('location')->nullable()->default('N/A');  
             $table->string('assigned')->nullable(); // Optional field
             $table->timestamps();
