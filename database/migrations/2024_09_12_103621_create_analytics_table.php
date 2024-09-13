cce01bb2ceeca4d84d3381eeb7cb4ba7e984f221
NarukamiXV <apcmendiola.student@ua.edu.ph>
2024-09-13 21:09:51 +0800

    Create 2024_09_12_103621_create_analytics_table.php

---
+++ database/migrations/2024_09_12_103621_create_analytics_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('analytics', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->enum('metric', ['ticket_history', 'ticket_queue', 'tickets_resolved', 'created_tickets', 'accepted_tickets']);
+            $table->unsignedInteger('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('analytics');
+    }
+};
