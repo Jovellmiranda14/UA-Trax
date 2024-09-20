839e4074f5404bff672bfa1c23a2e6bb5dda8988
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-09-20 13:11:16 +0800

    updated routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Filament\Auth\Login;
 
+/*<?php
+
+use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
- Route::get('/', function () {
-      return view('welcome');
-  });
 
+return the login.php to the web.php
+ */
+
+// Redirect the root ('/') route to the login page
+Route::get('/', function () {
+    return redirect('/user/login');
+});
+
+// Define the login route to point to your custom login logic
+// Route::get('/login', [Login::class, 'render'])
+//     ->name('login');
 
   use App\Http\Controllers\TicketController;
 
