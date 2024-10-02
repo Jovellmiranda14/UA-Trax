3d5827a317bb970bcd308a6a2683c1b1a3cc42ab
NarukamiXV <apcmendiola.student@ua.edu.ph>
2024-10-02 21:01:01 +0800

    LOL

--- app/Filament/Pages/AnalyticsChart.php
+++ app/Filament/Pages/AnalyticsChart.php
@@ -8,7 +8,6 @@ use App\Filament\Resources\TicketsAcceptedResource\Widgets\TicketAcceptedChart;
 use App\Filament\Resources\TicketResolvedResource\Widgets\TicketResolvedChart;
 use App\Filament\Resources\TicketResource\Widgets\ConcernTypeChart;
 use App\Filament\Resources\TicketResource\Widgets\IssueTypeChart;
-use Illuminate\Support\Facades\Gate;
 
 
 class AnalyticsChart extends Page
@@ -25,31 +24,5 @@ class AnalyticsChart extends Page
             ConcernTypeChart::class,
             IssueTypeChart::class,
         ];
-    }
-
-    public static function canView(): bool
-    {
-        // Allow access only to admin roles
-        return Gate::allows('viewAny', AnalyticsChartPolicy::class);
-    }
-
-    public function mount()
-    {
-        // Deny access if the user is not an admin
-        if (!self::canView()) {
-            abort(403); // Deny access for non-admin users
-        }
-    }
-
-    public static function getNavigationGroup(): ?string
-    {
-        // Only show in navigation if the user can view the page
-        return self::canView() ? 'Analytics' : null;
-    }
-
-    public static function getNavigationLabel(): string
-    {
-        // Always return this label as the page is for admins only
-        return 'Analytics Chart';
     }
 }
