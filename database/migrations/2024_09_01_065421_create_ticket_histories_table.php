0bf5041cef5a67b8204af2846216ce7ceb135cec
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-10-19 01:19:42 +0800

    Update 2024_09_01_065421_create_ticket_histories_table.php

--- database/migrations/2024_09_01_065421_create_ticket_histories_table.php
+++ database/migrations/2024_09_01_065421_create_ticket_histories_table.php
@@ -29,12 +29,12 @@ return new class extends Migration {
             $table->enum('status', ['Open', 'Resolved', 'In progress', 'Closed', 'On-Hold'])->default('Open'); // Column to store the status of the ticket
             $table->enum('priority', ['Moderate', 'Urgent', 'Low', 'High', 'Escalated'])->default('Moderate'); // Column to store the priority of the ticket
             $table->string('location')->nullable();
-            $table->enum('department', ['SAS', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE'])->nullable(); // Column to store the department related to the ticket
+            $table->enum('department', ['AB COMM', 'PSYCH', 'CRIM', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE']);
             $table->timestamps();
         
             $table->timestamp('accepted_at')->nullable();
-            $table->enum('dept_role', ['SAS', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE']);
-            $table->enum('position', ['RSO', 'Faculty', 'Secretary', 'N/A']);
+            $table->enum('dept_role', [ 'AB COMM', 'PSYCH', 'CRIM', 'SAS', 'CEA', 'CONP', 'CITCLS', 'OFFICE', 'PPGS']);
+            $table->enum('position', ['RSO', 'Faculty', 'Secretary', 'PPGS']);
             $table->string('assigned')->nullable();
             $table->binary('attachment')->nullable();
         });
