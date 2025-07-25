48f8f7b7d7274dbbf2bb5ea51ae389c4c2bb3064
Jovellmiranda <jovellmiranda14@gmail.com>
2025-07-25 17:02:18 +0800

    refactor: Simplify user_id foreign key definition and improve code formatting in tickets migration

--- database/migrations/2024_08_25_135109_create_tickets_accepted_table.php
+++ database/migrations/2024_08_25_135109_create_tickets_accepted_table.php
@@ -4,18 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('tickets_accepted', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->string('name');          
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
             $table->string('subject');
             $table->text('description')->nullable();
             $table->enum('type_of_issue', [
@@ -27,26 +22,22 @@ return new class extends Migration
                 'computer_issues',
                 'lab_equipment',
                 'Other_Devices',
-            ]); 
-            $table->enum('concern_type', ['Laboratory and Equipment', 'Facility']);        
-            $table->enum('status', ['Open', 'Resolved', 'In progress', 'Closed', 'On-Hold'])->default('Open');      
-            $table->enum('priority', ['Moderate', 'Urgent', 'Low', 'High','Escalated'])->default('Moderate');   
-            $table->enum('department', ['SAS (PSYCH)', 'SAS (CRIM)', 'SAS (AB COMM)','CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE']);
+            ]);
+            $table->enum('concern_type', ['Laboratory and Equipment', 'Facility']);
+            $table->enum('status', ['Open', 'Resolved', 'In progress', 'Closed', 'On-Hold'])->default('Open');
+            $table->enum('priority', ['Moderate', 'Urgent', 'Low', 'High', 'Escalated'])->default('Moderate');
+            $table->enum('department', ['SAS (PSYCH)', 'SAS (CRIM)', 'SAS (AB COMM)', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE']);
             $table->string('location')->nullable()->default('N/A');
-            $table->enum('dept_role', [ 'SAS (PSYCH)', 'SAS (CRIM)', 'SAS (AB COMM)', 'CEA', 'CONP', 'CITCLS', 'OFFICE', 'PPGS']);
-            $table->string('attachment')->nullable();      
-            $table->string('assigned')->nullable(); 
-            $table->enum('position', ['RSO', 'Faculty','Secretary', 'N/A']);
-            $table->timestamp('accepted_at'); 
-            $table->unsignedBigInteger('assigned_id')->nullable();
-            $table->foreign('assigned_id')->references('id')->on('users')->onDelete('cascade');
-            $table->timestamps();       
+            $table->enum('dept_role', ['SAS (PSYCH)', 'SAS (CRIM)', 'SAS (AB COMM)', 'CEA', 'CONP', 'CITCLS', 'OFFICE', 'PPGS']);
+            $table->string('attachment')->nullable();
+            $table->string('assigned')->nullable();
+            $table->enum('position', ['RSO', 'Faculty', 'Secretary', 'N/A']);
+            $table->timestamp('accepted_at');
+            $table->foreignID('assigned_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tickets_accepted');
