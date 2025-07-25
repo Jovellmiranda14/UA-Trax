6416c4f2ed85fd48d18cb56f952776fcc5618cb7
Jovellmiranda <jovellmiranda14@gmail.com>
2025-07-25 17:02:41 +0800

    refactor: Change primary key in departments migration from auto-incrementing ID to UUID

--- database/migrations/2024_12_15_192311_create_departments_table.php
+++ database/migrations/2024_12_15_192311_create_departments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('departments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name')->unique(); // Department name
             $table->string('code')->unique(); // Short code for the department
             $table->string('building')->nullable(); 
