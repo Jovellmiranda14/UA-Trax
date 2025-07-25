05fabbf7ab58c511700c73959ba88c25d3967a57
Jovellmiranda <jovellmiranda14@gmail.com>
2025-07-25 15:04:32 +0800

    cleanu

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Filament\Auth\Login;
 
-/*<?php
-
-use Illuminate\Support\Facades\Route;
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-return the login.php to the web.php
- */
 
 // Redirect the root ('/') route to the login page
 Route::get('/', function () {
     return redirect('/login');
 });
-
-// Define the login route to point to your custom login logic
-// Route::get('/login', [Login::class, 'render'])
-//     ->name('login');
-
-  // use App\Http\Controllers\TicketController;
-
-  // Route::get('/tickets/{id}', [TicketController::class, 'show']);
