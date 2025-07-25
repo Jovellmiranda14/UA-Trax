bd8041c2a58c7cf536f6e60819a72a77cc177011
Jovellmiranda <jovellmiranda14@gmail.com>
2025-07-25 17:02:25 +0800

    refactor: Simplify user_id and assigned_id foreign key definitions in tickets_resolved migration

--- database/migrations/2024_08_26_152530_create_tickets_resolved_table.php
+++ database/migrations/2024_08_26_152530_create_tickets_resolved_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,7 @@ return new class extends Migration
     {
         Schema::create('tickets_resolved', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->string('name');
             $table->string('subject');
             $table->text('description')->nullable();
@@ -27,18 +25,17 @@ return new class extends Migration
                 'computer_issues',
                 'lab_equipment',
                 'Other_Devices',
-            ]); 
-            $table->enum('concern_type', ['Laboratory and Equipment', 'Facility']);  
-            $table->enum('status', ['Open', 'Resolved', 'In progress', 'Closed' , 'On-Hold'])->default('Open')->nullable();
-            $table->enum('priority', ['Moderate', 'Urgent', 'Low', 'High','Escalated'])->default('Moderate');
+            ]);
+            $table->enum('concern_type', ['Laboratory and Equipment', 'Facility']);
+            $table->enum('status', ['Open', 'Resolved', 'In progress', 'Closed', 'On-Hold'])->default('Open')->nullable();
+            $table->enum('priority', ['Moderate', 'Urgent', 'Low', 'High', 'Escalated'])->default('Moderate');
             $table->enum('department', ['SAS (PSYCH)', 'SAS (CRIM)', 'SAS (AB COMM)', 'CEA', 'CONP', 'CITCLS', 'RSO', 'OFFICE']);
-            $table->string('location')->default('N/A');  
-            $table->string('attachment')->nullable();  
+            $table->string('location')->default('N/A');
+            $table->string('attachment')->nullable();
             $table->string('assigned')->nullable();
             $table->enum('dept_role', ['SAS (PSYCH)', 'SAS (CRIM)', 'SAS (AB COMM)', 'CEA', 'CONP', 'CITCLS', 'OFFICE', 'PPGS']);
             $table->timestamps();
-            $table->unsignedBigInteger('assigned_id')->nullable();
-            $table->foreign('assigned_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignID('assigned_id')->references('id')->on('users')->onDelete('cascade') ->nullable();
             $table->timestamp('resolved_at')->nullable();
             $table->timestamp('accepted_at')->nullable();
         });
